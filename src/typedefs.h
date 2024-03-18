aeff4ff92637012784da87c5bae877c30c3f2e2e
iridiumpixel <rogomaev@yandex.ru>
2024-03-18 07:53:48 +0300

    added basic definitions

---
+++ src/typedefs.h
@@ -0,0 +1,32 @@
+#ifndef _TYPEDEFS_H
+#define _TYPEDEFS_H
+
+#include "defines.h"
+#include "stdint.h"
+
+typedef struct 
+{
+  // Memory
+  uint8_t PROGRAM_MEMORY[PROGRAM_MEMORY_SIZE];
+  uint8_t SRAM[SRAM_SIZE];
+  uint8_t PC; // Program counter
+  uint8_t SP; // Stack pointer
+
+  uint64_t tick_counter;
+
+  // ¨Pins¨ for debugging
+  uint8_t PORTA;
+  uint8_t PORTB;
+  uint8_t PORTC;
+} ATmega328p;
+
+typedef struct
+{
+  void (*instruction_handler)(ATmega328p* MCU, uint32_t opcode);
+  uint32_t opcode;
+  uint32_t opcode_mask; 
+  uint8_t  length;
+  uint8_t  cycles;
+} Instruction;
+
+#endif
